150d3e0a60bce7adb23e7ef453519a8e9ae7cea5
Karthikeyan Ravi <karthikeyan.ravi11@hotmail.com>
2020-02-13 18:22:46 +0530

    Add project files.

---
+++ Restaurant/Controllers/RestaurantController.cs
@@ -0,0 +1,39 @@
+﻿using RestaurantDAL;
+using RestaurantEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Restaurant.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        // GET: Restaurant
+        RestaurantRepository restaurantRepository = new RestaurantRepository();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult DisplayRestaurantDetails()
+        {
+            
+            IEnumerable<RestaurantFields> restaurantList =restaurantRepository.DisplayRestaurantFields();
+            ViewBag.RestaurantDetails = restaurantList;
+            return View();
+        }
+        public ActionResult ViewRestaurantDetails()
+        {
+            IEnumerable<RestaurantFields> viewRestaurantList = restaurantRepository.DisplayRestaurantFields();
+            ViewData["RestaurantDetails"] = viewRestaurantList;
+            return View();
+        }
+        public ActionResult TempDataResult()
+        {
+            IEnumerable<RestaurantFields> RestaurantList = restaurantRepository.DisplayRestaurantFields();
+            TempData["RestaurantDetails"] = RestaurantList;
+            return View();
+        }
+    }
+}
---
+++ RestaurantDAL/RestaurantRepository.cs
@@ -0,0 +1,27 @@
+﻿using RestaurantEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantDAL
+{
+    public class RestaurantRepository
+    {
+        static List<RestaurantFields> restaurantDetails = new List<RestaurantFields>();
+        static RestaurantRepository()
+        {
+            RestaurantFields restaurantFields = new RestaurantFields("HMR", "Soth-Indian, North Indian", "R.S Puram, Cbe");
+            restaurantDetails.Add(restaurantFields);
+            restaurantFields = new RestaurantFields("AAchis", "Chinese, North Indian", "Race course, Cbe");
+            restaurantDetails.Add(restaurantFields);
+            restaurantFields = new RestaurantFields("Monkey Restaurant", "South-Indian,North-Indian", "Hopes, Cbe");
+            restaurantDetails.Add(restaurantFields);
+        }
+        public IEnumerable<RestaurantFields> DisplayRestaurantFields()
+        {
+            return restaurantDetails;
+        }
+    }
+}
