d9d1be23131fc4358cd2e85cff364b3441858a2d
Karthikeyan Ravi <karthikeyan.ravi11@gmail.com>
2020-02-23 18:52:13 +0530

    Add files via upload

---
+++ Controllers/RestaurantController.cs
@@ -0,0 +1,63 @@
+﻿using RestaurantDAL;
+using RestaurantEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Restaurant.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        // GET: Restaurant
+        RestaurantRepository restaurantRepository = new RestaurantRepository();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult DisplayRestaurantDetails()
+        {
+            
+            IEnumerable<RestaurantFields> restaurantList =restaurantRepository.DisplayRestaurantFields();
+            ViewBag.RestaurantDetails = restaurantList;
+            return View();
+        }
+        public ActionResult ViewRestaurantDetails()
+        {
+            IEnumerable<RestaurantFields> viewRestaurantList = restaurantRepository.DisplayRestaurantFields();
+            ViewData["RestaurantDetails"] = viewRestaurantList;
+            return View();
+        }
+        public ActionResult TempDataResult() { 
+            IEnumerable<RestaurantFields> RestaurantList = restaurantRepository.DisplayRestaurantFields();
+            TempData["RestaurantDetails"] = RestaurantList;
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create([Bind(Include ="RestaurantName,Location")]RestaurantFields restaurantFields)
+        {
+            restaurantRepository.Add(restaurantFields);
+            return RedirectToAction("TempDataResult");
+        }
+        public ActionResult Delete(int id)
+        {
+            restaurantRepository.Delete(id);
+            return RedirectToAction("TempDataResult");
+        }
+        public ActionResult Edit(int id)
+        {
+            RestaurantFields restaurantFields = restaurantRepository.GetRestaurantById(id);
+            return View(restaurantFields);
+        }
+        public ActionResult Update([Bind(Exclude ="RestaurantType")]RestaurantFields restaurantFields)
+        {
+            restaurantRepository.Update(restaurantFields);
+            return RedirectToAction("TempdataResult");
+        }
+    }
+}
